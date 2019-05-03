6beec92eb8753c0c847c556aea7ed9a1144dc3f0
LaDonna Moaney <ladonnamoney@gmail.com>
2019-05-02 23:16:55 -0400

    added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+//MOUSEOVER
+
+const links = document.querySelector('nav');
+links.addEventListener('mouseover', function(e) {
+    e.target.style.color = 'gold';
+
+    setTimeout(function() {
+        e.target.style.color = '';
+    }, 500);
+}, false);
+
+//Click Event
+
+const button = document.querySelectorAll('.btn');
+button.forEach(button => {
+  button.addEventListener('click', e => {
+    e.target.style.color = 'green';
+  });
+});
